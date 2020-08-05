c6f273c5ca5d991e011da538738d7828001f3450
Richard Rosario <tech2js@gmail.com>
2020-08-05 14:07:27 +0800

    setup application  port

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => {
+  return res.send("Hello world");
+});
+
+app.listen(5060, () => {
+  console.log("Server started at port 5060");
+});
