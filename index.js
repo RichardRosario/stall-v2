79eb0e2a5b868c9ea78f62cfbd5d1242ce36ee4e
Richard Rosario <tech2js@gmail.com>
2020-08-05 20:11:11 +0800

    install eslint to manage code structure

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const helmet = require("helmet");
-const https = require("https");
 
 const app = express();
-app.use(helmet()); //for helmet as middlewaht to force https
 
 app.get("/", (req, res) => {
   return res.send("Hello world");
@@ -12,5 +9,3 @@ app.get("/", (req, res) => {
 app.listen(5060, () => {
   console.log("Server started at port 5060");
 });
-
-https.createServer(app).listen(5070);
