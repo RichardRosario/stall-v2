e5cbc3eee48e783466ff227d076dd72c0203d899
Richard Rosario <tech2js@gmail.com>
2020-08-09 00:57:46 +0800

    implemented ejs for templating/views

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  return res.send("Hello world");
+  return res.render("index");
 });
 
 app.listen(8080, () => {
